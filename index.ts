42a87c6232eb4823618c49873994233a2fee61e0
Dwipa Yogiswara <dwipayogi50@gmail.com>
2025-01-05 12:55:06 +0700

    initialize project

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+// Import Route
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+// Define Route
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
